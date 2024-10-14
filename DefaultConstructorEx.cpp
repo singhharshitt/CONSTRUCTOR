eb9f667189eee608d0a3e5310f8b524a240826da
Harshit singh <149008610+singhharshitt@users.noreply.github.com>
2024-10-14 17:18:09 +0530

    Update DefaultConstructorEx.cpp

--- DefaultConstructorEx.cpp
+++ DefaultConstructorEx.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class car{
+    string brand;
+    string model;
+    int year;
+    public:
+    car(){
+        brand= "Unknown";
+        model= "Generic";
+        year=2000;
+    }
+    void displayCarInfo(){
+        cout<<"Here, The Car Details: "<<endl;
+        cout<<"Brand: "<<brand<<endl;
+        cout<<"Model: "<<model<<endl;
+        cout<<"Year: "<<year<<endl;
+    }
+};
+int main(){
+    car c;
+    c.displayCarInfo();
+    return 0;
+}
