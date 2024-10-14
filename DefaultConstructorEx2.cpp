75ab9ba1846ea9b52da4394b0794135dedd0fcc0
Harshit singh <149008610+singhharshitt@users.noreply.github.com>
2024-10-14 17:26:39 +0530

    Update DefaultConstructorEx2.cpp

--- DefaultConstructorEx2.cpp
+++ DefaultConstructorEx2.cpp
@@ -1,1 +1,25 @@
-
+#include <iostream>
+using namespace std;
+class Book{
+    string title;
+    string author;
+    float price;
+    public:
+    Book(){
+        title="Time story";
+        author = "Sam D Kenny";
+        price=275.50;
+    }
+    void showDetails(){
+        cout<<"Books Details are-->"<<endl;
+        cout<<"Title: "<<title<<endl;
+        cout<<"Author: "<<author<<endl;
+        cout<<"Price: "<<price<<endl;
+    }
+    
+};
+int main(){
+    Book b;
+    b.showDetails();
+    return 0;
+}
